0ef36efccffa093d8a1fdc14d7fc81ff93cecfb5
darkCoderMahesh <141926871+darkCoderMahesh@users.noreply.github.com>
2024-02-26 18:15:41 +0530

    Abhijit update

--- ORKIN_TERMITES _Banners/Termite_In-Market/300X250/script.js
+++ ORKIN_TERMITES _Banners/Termite_In-Market/300X250/script.js
@@ -6,43 +6,46 @@ var tl;
 
 // Init tricggered by onLoad in Body tag
 function init() {
-    // Set Banner duration timer
-    startTime = new Date();
+  // Set Banner duration timer
+  startTime = new Date();
 
-    // Set Global Timeline
-    tl = new TimelineMax({ onComplete: endTime });
-  
-    animate();
-  }
+  // Set Global Timeline
+  tl = new TimelineMax({ onComplete: endTime });
+
+  animate();
+}
 
 function animate() {
-    tl.set(["#main-container, #banner"], { autoAlpha: 1, force3D: true });
-    tl.set(["#photo1, #photo2"], { rotationZ: 0.1, force3D:true});
-    tl.set(["#photo2"], { x:-300});
-    
-    tl.addLabel('frame_1', 0)
-      .to(photo1, 1, { x:"0", autoAlpha:1, ease:"none" }, "frame_1")
-      .to(copy1, 0.5, {left:15, autoAlpha:1, ease: "power1.out", }, "frame_1+=0.5")
-      .to(copy1, 0.5, {x:'-120', autoAlpha:0, ease: "power1.out", }, "frame_1+=3");
+  tl.set(["#main-container, #banner"], { autoAlpha: 1, force3D: true });
+  tl.set(["#photo1, #photo2"], { rotationZ: 0.1, force3D: true });
+  tl.set(["#photo2"], { x: -300 });
 
-    tl.addLabel('frame_2', 3.5)
-      .to(copy2, 0.5, {left:15, autoAlpha:1, ease: "power1.out", }, "frame_2")
-      // .to(photo1, 1, {autoAlpha:0, ease: 'power4.inOut', }, "frame_2")
-      .to(photo2, 0.8, {autoAlpha:1, x:"0", ease:"none"}, "frame_2")
-      .to(green_angle, 0.5, {autoAlpha:1, ease: "power1.out", y:'0'}, "frame_2")
-      .to(copy2, 0.5, {x:'-120', autoAlpha:0, ease: "power1.out", }, "frame_2+=3")
-    
-    
-    tl.addLabel('frame_3', 7)
-      .to(copy3, 0.5, {left:15, autoAlpha:1, ease: "power1.out", }, "frame_3")
-      .to(["#cta"], 0.5, {bottom:20, autoAlpha:1, ease: "power1.out", }, "<")
-      .to(terms, {autoAlpha:1})
+  tl.addLabel('frame_1', 0)
+    .to(photo1, 1, { x: "0", autoAlpha: 1, ease: "none" }, "frame_1")
+    .to(copy1, 0.5, { left: 15, autoAlpha: 1, ease: "power1.out", }, "frame_1+=0.5")
+    .to(photo1, 3, { scale: 1.1, x: '-5', y: '10', autoAlpha: 1, ease: "none" }, "frame_1+=1")
+    .to(copy1, 0.5, { x: '-120', autoAlpha: 0, ease: "power1.out", }, "frame_1+=3");
+
+  tl.addLabel('frame_2', 3.5)
+    .to(copy2, 0.5, { left: 15, autoAlpha: 1, ease: "power1.out", }, "frame_2")
+    .to(photo2, 0.8, { autoAlpha: 1, x: "0", ease: "none" }, "frame_2")
+    .to(green_angle, 0.5, { autoAlpha: 1, ease: "power1.out", y: '0' }, "frame_2")
+    .to(photo2, 5.5, { scale: 1.1, x: '-15', y: '-10', autoAlpha: 1, ease: "none" }, "frame_2+=0.8")
+
+
+  tl.addLabel('frame_3', 7)
+    .to(["#cta"], 0.5, { bottom: 30, autoAlpha: 1, ease: "power1.out", }, "frame_3")
+    .to(copy2, 0.2, { y: '-20', opacity: 0, autoAlpha: 0, ease: "power1.out", }, "frame_3+=0.5")
+    .to(copy3, 0, { left: 15, y: '-20', autoAlpha: 1, ease: "power1.out", }, "frame_3+=0.7")
+    .to(copy3, 0.2, { left: 15, y: '0', autoAlpha: 1, ease: "power1.out", }, "frame_3+=0.7")
+    .to(["#cta"], 0.2, { bottom: 20, autoAlpha: 1, ease: "power1.out", }, "frame_3+=0.9")
+    .to(terms, { autoAlpha: 1 })
 }
 
 function endTime() {
-    // show total banner animation time in browser console.
-    var endTime = new Date();
-    console.log(
-      "Animation duration: " + (endTime - startTime) / 1000 + " seconds"
-    );
-  }
+  // show total banner animation time in browser console.
+  var endTime = new Date();
+  console.log(
+    "Animation duration: " + (endTime - startTime) / 1000 + " seconds"
+  );
+}
