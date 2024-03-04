da197fdc033040c0521d273e44b6b588ab9460e4
AbhijithCSundaran <abhijith.sundaran@annalect.com>
2024-03-05 01:48:06 +0530

    728x90 size reduced

--- Orkin2024/Orkin_Termites_InMarket/Orkin_Termites_InMarket_728X90/scripts/script.js
+++ Orkin2024/Orkin_Termites_InMarket/Orkin_Termites_InMarket_728X90/scripts/script.js
@@ -22,7 +22,7 @@ function frame_one() {
 }
 
 function frame_two() {
-	gsap.to("#bg2", 10, { x: 20, y: -40, scale: 1, ease: Linear.easeNone });
+	gsap.to("#bg2", 10, { x: 30, y: -60, scale: 1, ease: Linear.easeNone });
 	gsap.to("#green_angle", 1.5, { autoAlpha: 1, y: '0', ease: Linear.easeNone })
 	gsap.to("#copy1", 0.6, { alpha: 0, ease: Power2.easeOut });
 	gsap.to("#bg1", 0.6, { alpha: 0, delay: 0.25, ease: Power2.easeOut });
